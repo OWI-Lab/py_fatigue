de46f0139a353421efaacb866a864dc4c74a98e7
Pietro D'Antuono <pietro.dantuono@vub.be>
2022-12-21 20:04:18 +0100

    Bumpversion 1.0.8

--- py_fatigue/version.py
+++ py_fatigue/version.py
@@ -17,7 +17,7 @@ Attributes:
 import re
 from typing import Optional, NamedTuple
 
-__version__: str = "1.0.7"
+__version__: str = "1.0.8"
 
 _REGEX = (
     r"(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)"
