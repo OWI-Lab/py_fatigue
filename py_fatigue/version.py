0330f532530de0820499f416ab6ebd9803d5305d
Pietro D'Antuono <pietro.dantuono@vub.be>
2023-01-02 16:00:32 +0100

    fix: pkg: Updated lock to solve mypy bug

--- py_fatigue/version.py
+++ py_fatigue/version.py
@@ -17,7 +17,7 @@ Attributes:
 import re
 from typing import Optional, NamedTuple
 
-__version__: str = "1.0.16"
+__version__: str = "1.0.17"
 
 _REGEX = (
     r"(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)"
