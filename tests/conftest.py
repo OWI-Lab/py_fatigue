6b57675bb86a58bd2e6676d56f9a223b1060efbe
Pietro D'Antuono <pietro.dantuono@24sea.eu>
2025-02-21 17:57:43 +0100

    ci(bump): Add bump step to cicd pipeline [skip ci]

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from distutils import dir_util
 
 logger = logging.getLogger(__name__)
 
-VERSION = "1.2.0"
+VERSION: str = "1.2.0"
 
 # Add custom options to pytest. Here we define the addition of an 'url' argument
 # so you could run `pytest --url <url>` to specify an external url to check real time
