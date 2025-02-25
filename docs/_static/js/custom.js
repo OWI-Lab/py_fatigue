17ccca478c1a75ca34c5182fd97d61d4f9643ec5
Pietro D'Antuono <pietro.dantuono@24sea.eu>
2025-02-25 05:49:51 +0100

    feat(nonlinear): Add Theil and Pavlou (DCA) stress-life methods

--- docs/_static/js/custom.js
+++
@@ -1,9 +0,0 @@
-MathJax.Hub.Config({
-    "HTML-CSS": {
-      availableFonts: [],
-      preferredFonts: "'Neo euler', TeX",
-      webFont:"",
-      imageFont:"",
-      undefinedFamily:"'Roboto Slab', 'Neo Euler', serif"
-    }
-  });
